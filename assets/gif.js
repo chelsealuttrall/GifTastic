170d5f206b23b50f40fb162a9cccf8c6b486f61e
chelsealuttrall <chelsealuttrall@gmail.com>
2020-05-08 19:40:50 -0600

    yep

--- assets/gif.js
+++ assets/gif.js
@@ -1,34 +1,27 @@
-var topics = ["mountain", "moon", "monster"]
+var topics = ["mountain", "moon", "monster"];
 
 //Makin' Button & Postin' Button
-let buttonMakerFxn = function() {
-    for (let i = 0; i < topics.length; i++) {
-        buttonMaker =
-            $('<button>' + topics[i] + '</button>').attr('data-name', topics[i]).attr('class', topics[i]);
-        buttonMaker.appendTo("#buttonZone");
-    } 
+let buttonMakerFxn = function () {
+  for (let i = 0; i < topics.length; i++) {
+    buttonMaker = $("<button>" + topics[i] + "</button>")
+      .attr("data-name", topics[i])
+      .attr("class", topics[i] + " buttons");
+    buttonMaker.appendTo("#buttonZone");
+  }
 };
 
 $(document).ready(buttonMakerFxn);
 
-
 //search bar button maker
-$('#submit').on("click", function() {
-    console.log("submit button works")
-    topics.push($("#searchBar").val())
-    $("#searchBar").val("");
-    console.log(topics);
-   $("#buttonZone").empty();
-    buttonMakerFxn()
+$("#submit").on("click", function () {
+  console.log("submit button works");
+  topics.push($("#searchBar").val());
+  $("#searchBar").val("");
+  console.log(topics);
+  $("#buttonZone").empty();
+  buttonMakerFxn();
 });
 
-$("#buttonZone".children).on("click", function() {
-    console.log("clicked button");
-    // console.log($(this).attr('data-name'));
-    // searchWord = $(this).attr('data-name');
-    // console.log(searchWord)
-    // $("#searchResultsHTML").empty();
-});
 //click the button and initiate the search of the word
 let newImage;
 let searchWord;
@@ -38,33 +31,38 @@ let searchWord;
 //     searchWord = $(this).attr('data-name');
 //     console.log(searchWord)
 //     $("#searchResultsHTML").empty();
-    // return searchWord;
+// return searchWord;
 
-
+$(document).on("click", ".buttons", function () {
+  console.log("clicked button");
+  console.log($(this).data("name"));
+  searchWord = $(this).data("name");
+  console.log(searchWord);
+  $("#searchResultsHTML").empty();
+  gettingImages(searchWord);
+});
 //make function that query's API
 
-let gettingImages = () => {
-    let api_key = '&api_key=nQfFTQdcuEY4gT3kQ5tX7ORFubjTOi4z'
-    let resultLimit = '&limit=10'
-    var queryURL = 'https://api.giphy.com/v1/gifs/search?q='
-    queryURL = queryURL + searchWord + api_key + resultLimit;
-    console.log(queryURL)
-    $.ajax({
-            url: queryURL,
-            method: "GET",
-        })
-        .then(function(response) {
-            console.log(response)
-            for (let i = 0; i < response.data.length; i++) {
-                var giphyURL = response.data[i].images.fixed_height_still.url
-                newImage = $('<img>')
-                newImage.attr("src", giphyURL).attr("id", response.data[i].id);
-                $("#searchResultsHTML").append(newImage);
-
-            }
-        }); return queryURL;
+let gettingImages = (searchWord) => {
+  let api_key = "&api_key=nQfFTQdcuEY4gT3kQ5tX7ORFubjTOi4z";
+  let resultLimit = "&limit=10";
+  var queryURL = "https://api.giphy.com/v1/gifs/search?q=";
+  queryURL = queryURL + searchWord + api_key + resultLimit;
+  console.log(queryURL);
+  $.ajax({
+    url: queryURL,
+    method: "GET",
+  }).then(function (response) {
+    console.log(response);
+    for (let i = 0; i < response.data.length; i++) {
+      var giphyURL = response.data[i].images.fixed_height_still.url;
+      newImage = $("<img>");
+      newImage.attr("src", giphyURL).attr("id", response.data[i].id);
+      $("#searchResultsHTML").append(newImage);
+    }
+  });
+  return queryURL;
 };
-
 
 //$('<img/>').click.attr("src", this + '.images.fixed_height.url');
 //     title: 'Delete ' + fotos[f].Title
